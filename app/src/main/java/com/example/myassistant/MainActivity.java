1c7c1f8a312dbf74d0dbeb50fc99e5e893f139de
Noa Fatael <noafatael@gmail.com>
2021-03-12 17:04:15 +0200

    adding on click

--- app/src/main/java/com/example/myassistant/MainActivity.java
+++ app/src/main/java/com/example/myassistant/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.myassistant;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnStart, btnStop;
+    private Context main_activity_context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init buttons
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
+        btnStart.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        
     }
 }
